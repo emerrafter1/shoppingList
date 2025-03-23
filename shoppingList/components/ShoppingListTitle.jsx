d799c2ee7e46ce1845060806186485108746688e
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-23 12:05:33 +0000

    add edit list title

--- shoppingList/components/ShoppingListTitle.jsx
+++ shoppingList/components/ShoppingListTitle.jsx
@@ -1,10 +1,45 @@
 import { useState } from "react";
 function ShoppingListTitle() {
   const [listName, setListName] = useState("My Shopping List");
+  const [isEditingListName, setIsEditingListName] = useState(false);
+
+  function handleEditClick() {
+    setIsEditingListName(true);
+  }
+
+  function handleInputChange(event) {
+    setListName(event.target.value);
+  }
+
+  function handleBlurChange() {
+    setIsEditingListName(false);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      setIsEditingListName(false);
+    }
+  }
 
   return (
-    <div>
-      <h2>{listName}</h2>
+    <div className="shopping-list-title">
+      {isEditingListName ? (
+        <input
+        className="edit-list-name"
+          type="text"
+          value={listName}
+          onChange={handleInputChange}
+          onBlur={handleBlurChange}
+          onKeyDown={handleKeyDown}
+        ></input>
+      ) : (
+        <div className="shopping-list-title-row">
+          <h2>{listName}</h2>
+          <button className="edit-button" onClick={handleEditClick}>
+            ✎
+          </button>
+        </div>
+      )}
     </div>
   );
 }
